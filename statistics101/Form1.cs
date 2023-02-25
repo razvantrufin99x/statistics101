bc6c63f83b2df507c58ae0d2edefb130c42ccd82
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-02-25 12:48:34 +0200

    Add project files.

---
+++ statistics101/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace statistics101
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statistics s = new statistics();
+            Text = s.Radjsqr(0.5, 50, 5).ToString();
+            textBox1.Text = Text;
+            Text += " : ";
+            Text += s.testRadjsqr(0.5, 50, 5, 0.164772727272727).ToString();
+        }
+    }
+}
---
+++ statistics101/statistics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statistics101
+{
+    public class statistics
+    {
+
+
+
+        //Adjuster R Squared
+        //proportion of the variation in your Y explained by X for a linear regression model
+        //how the well terms fit a curve or line 
+        //n nr of points in your data sample seize sample  = 50
+        //k nr of independent regressors predictors sample = 5
+        //R*R sample = 0.5 
+        //response 0.1648
+
+        //how to use this Radjsqr(0.5, 50, 5)
+        public double Radjsqr(double R, double n, double k)
+        {
+
+            return (1 - (((1 - R * R) * (n - 1)) / (n - k - 1)));
+
+        }
+
+        //test the function above
+        //how to use this 
+        //testRadjsqr(0.5 , 50, 5 , 0.164772727272727)
+        //corect 0.164772727272727
+        public bool testRadjsqr(double R, double n, double k, double response)
+        {
+
+            if (this.Radjsqr(0.5, 50, 5) == response || this.Radjsqr(0.5, 50, 5) - response<0.000000001) return true;
+            else { return false; }
+                 
+        }
+
+
+    }
+}
